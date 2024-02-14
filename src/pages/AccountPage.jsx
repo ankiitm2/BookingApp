24d7f5b64d4a293cc007731aa5568dbd25e2419e
Ankit Mishra <ankit.mishra@paavu.com>
2024-02-14 18:47:57 +0530

    changed

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -4,6 +4,9 @@ import { Link, useParams } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 import axios from 'axios';
 import PlacesPage from './placespage/PlacesPage';
+import { FaUserLarge } from "react-icons/fa6";
+import { FaCalendarCheck } from "react-icons/fa";
+import { FaBuilding } from "react-icons/fa";
 
 const AccountPage = () => {
     const [redirect, setRedirect] = useState(null);
@@ -29,7 +32,7 @@ const AccountPage = () => {
 
 
     function linkClasses(type = null) {
-        let classes = 'py-2 px-6 font-medium';
+        let classes = 'inline-flex items-center gap-1 py-2 px-6 font-medium';
         if (type === subpage) {
             classes += ' bg-primary text-white rounded-full'
         }
@@ -43,14 +46,14 @@ const AccountPage = () => {
     return (
         <div className='account mt-4'>
             <nav className='flex justify-center gap-2'>
-                <Link className={linkClasses('profile')} to={"/account"}>My Profile</Link>
-                <Link className={linkClasses('bookings')} to={"/account/bookings"}>My Bookings</Link>
-                <Link className={linkClasses('places')} to={"/account/places"}>My Accommodation</Link>
+                <Link className={linkClasses('profile')} to={"/account"}><FaUserLarge />My Profile</Link>
+                <Link className={linkClasses('bookings')} to={"/account/bookings"}><FaCalendarCheck />My Bookings</Link>
+                <Link className={linkClasses('places')} to={"/account/places"}><FaBuilding />My Accommodation</Link>
             </nav>
             {subpage === 'profile' && (
-                <div className="flex flex-col mx-auto max-w-lg mt-3">
+                <div className="flex flex-col mx-auto max-w-lg mt-8">
                     <div className="text text-center font-medium">Logged in as <span className='capitalize'>{user.name}</span> ({user.email})</div>
-                    <button onClick={logout} className='bg-primary text-white rounded-full py-2 mt-2'>Log out</button>
+                    <button onClick={logout} className='bg-primary text-white rounded-full py-2 px-6 m-auto mt-2 w-fit '>Log out</button>
                 </div>
             )}
             {subpage === 'places' && (
--- src/pages/placespage/PlacesPage.jsx
+++ src/pages/placespage/PlacesPage.jsx
@@ -3,8 +3,8 @@ import { FaPlus } from "react-icons/fa";
 
 const PlacesPage = () => {
     return (
-        <div className="PlacesPage text-center mt-4">
-            <button className="bg-primary text-white font-medium px-6 py-2 rounded-full inline-flex items-center gap-2"><FaPlus /> Add new</button>
+        <div className="PlacesPage text-center mt-8">
+            <button className="bg-primary text-white font-medium px-6 py-2 rounded-full inline-flex items-center gap-2"><FaPlus /> Add new place</button>
         </div>
     )
 }
